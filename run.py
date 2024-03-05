b57ff9dd0d23e8878b4fc1853417a365fdad2c79
Yuri3 <konautomaton@gmail.com>
2024-03-05 10:28:33 +0800

    change load data path and output path

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 import os
-import pexpect
 
 method = str(input())
 
-input_file = 'data/nms_yolov8m_gpu/'
+input_file = '/data02/sl/nms_data/nms_yolov8m_gpu/'
 # gpu / cpu dataset is a little different, pay attention
-output_file = 'data/nms_yolov8m_output' + '_' + method + '/' 
+output_file = 'result/nms_yolov8m_output' + '_' + method + '/' 
 
 
 command = './build/output/main' + ' ' + input_file + ' ' + output_file  + ' ' + method
@@ -13,9 +12,3 @@ os.system(command)
 
 
 print("[test done!]")
-	
-# transfer_command = 'sudo -S scp -rC ' + output_file + ' '+ 'sjs@10.181.8.182:/data01/sl/nms_output'  
-# print(transfer_command)
-
-# os.system(transfer_command)
-# print("[transfer done!]")
