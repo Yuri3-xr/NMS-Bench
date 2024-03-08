2e5d511d4e85afba0a3ef8ef02a4d87b881657dd
Yuri3 <konautomaton@gmail.com>
2024-03-08 10:43:48 +0800

    add crowd human dataset

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import os
 
 method = str(input())
 
-input_file = '/data02/sl/nms_data/nms_yolov8m_gpu/'
+input_file = '/data02/sl/nms_data/nms_yolov8n_crowdhuman_gpu/'
 # gpu / cpu dataset is a little different, pay attention
-output_file = 'result/nms_yolov8m_output' + '_' + method + '/' 
+output_file = 'result/nms_yolov8n_crowdhuman_output' + '_' + method + '/' 
 
 
 command = './build/output/main' + ' ' + input_file + ' ' + output_file  + ' ' + method
