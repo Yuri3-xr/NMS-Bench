abf6ee0fc4415ab43ca05253897c856f099f9e2b
Yuri3 <konautomaton@gmail.com>
2024-04-25 22:48:57 +0800

    chang file path for exp

--- exp.py
+++ exp.py
@@ -2,7 +2,7 @@ import subprocess
 import re
 
 methods = ['GreedyNMS', 'BobNMS', 'AliceNMS', 'UltraAliceNMS']
-sizes = ['s', 'm', 'l', 'x']
+sizes = ['n', 's', 'm']
 
 results = {}
 
@@ -12,13 +12,15 @@ for method in methods:
         avg_map_50_95 = 0
         
         for _ in range(3):
-            command = f"python run.py --preds ./data/coco/yolo-series/v8{size}-preds/ --method {method}"
+            command = f"python run.py --preds ./data/open-images/yolo-series/v8{size}-preds/ --labels /data02/sl/open-images-v7-gt-csv/ --method {method}"
             output = subprocess.check_output(command, shell=True, universal_newlines=True)
             
             # Extract process time and mAP 50:95
-            process_time = re.search(r'process time is (\d+\.\d+) ms', output)
+            process_time = re.search(r'process time is (\d+(\.\d+)?) ms', output)
             map_50_95 = re.search(r'mAP 50:95\s+=\s+([\d.]+)', output)
             
+            print(output)
+            print(float(process_time.group(1)))
             if process_time and map_50_95:
                 avg_process_time += float(process_time.group(1))
                 avg_map_50_95 += float(map_50_95.group(1))
